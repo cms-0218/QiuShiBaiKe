9553546f6f33a50e88a548a439972e5bf0aeea34
cms-0218 <caomisheng@163.com>
2015-12-12 10:28:48 +0800

    add MainViewController.swift

---
+++ QiuShiBaiKe/QiuShiBaiKe/ViewControllers/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  QiuShiBaiKe
+//
+//  Created by andyyz on 15/12/12.
+//  Copyright © 2015年 andyyz. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController : UITabBarController {
+    
+}
