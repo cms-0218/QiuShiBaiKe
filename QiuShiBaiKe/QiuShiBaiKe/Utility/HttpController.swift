de1f2a456400bc9d68ada4b5012ad64cecbe828f
cms-0218 <caomisheng@163.com>
2015-12-13 19:48:22 +0800

    可以正常显示数据了，接下来就是细化功能

--- QiuShiBaiKe/QiuShiBaiKe/Utility/HttpController.swift
+++ QiuShiBaiKe/QiuShiBaiKe/Utility/HttpController.swift
@@ -20,37 +20,34 @@ class HttpController: NSObject {
     
     class func requestWithURL(urlString:String,completionHandler:(data:AnyObject)->Void)
     {
-        let URL = NSURL(string: urlString);
+        let URL = NSURL(string: urlString)
         let req = NSURLRequest(URL: URL!)
-        //let queue = NSOperationQueue();
         
         let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
         
-      
         let task : NSURLSessionTask = session.dataTaskWithRequest(req, completionHandler: { (data, response, error) -> Void in
             
-            if(error != nil)
-            {
-                dispatch_async(dispatch_get_main_queue(),
-                    {
-                        print(error)
-                        completionHandler(data:NSNull())
-                })
-
-            }
-            else
-            {
-                let jsonData = (try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
-                
-                dispatch_async(dispatch_get_main_queue(),
-                    {
-                        completionHandler(data:jsonData)
-                        
-                })
-
-            }
-        })
+                if(error != nil)
+                {
+                    dispatch_async(dispatch_get_main_queue(),
+                        {
+                            print(error)
+                            completionHandler(data:NSNull())
+                    })
+                }
+                else
+                {
+                    let jsonData = (try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
+                    
+                    dispatch_async(dispatch_get_main_queue(),
+                        {
+                            completionHandler(data:jsonData)
+                            
+                    })
+                }
+      
+            })
+        
         task.resume()
     }
-    
 }
